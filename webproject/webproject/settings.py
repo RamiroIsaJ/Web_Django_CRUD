da19eee2b4c8b8af294225161f380197e4d72fe2
Ramiro F. Isa-Jara <36931077+RamiroIsaJ@users.noreply.github.com>
2022-09-20 11:37:30 -0300

    Update settings.py

--- webproject/webproject/settings.py
+++ webproject/webproject/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'libreria',
         'USER': 'root',
-        'PASSWORD': 'Viper019',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306'
     }
